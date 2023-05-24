759c56ae2a9fe61074f702e83b9a1289194e4d36
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-05-24 14:12:58 +0700

    Update character selection

--- Assets/Script/Controller/Main_Controller.cs
+++ Assets/Script/Controller/Main_Controller.cs
@@ -5,32 +5,59 @@ using UnityEngine;
 public class Main_Controller : MonoBehaviour
 {
     private Main_Character_Unit selectedUnit;
+    private bool isDoubleTap = false;
+    private float doubleTapTimeThreshold = 0.2f; // Adjust this value to set the double tap time threshold
+    private float doubleTapTimer = 0f;
 
-    // Start is called before the first frame updat
-    private void Awake() {
-        selectedUnit = null;
-    } 
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.touchCount > 0){
-            DeselectUnit();
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out RaycastHit raycastHit)) {
-                if(raycastHit.collider.TryGetComponent<Main_Character_Unit>(out Main_Character_Unit main_Character_Unit)) {
-                    selectedUnit = main_Character_Unit;
+        if (Input.touchCount == 1 && selectedUnit)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (!isDoubleTap)
+                {
+                    isDoubleTap = true;
+                    doubleTapTimer = Time.time;
+                }
+                else
+                {
+                    if (Time.time - doubleTapTimer <= doubleTapTimeThreshold)
+                    {
+                        // Double tap detected
+                        selectedUnit.SetDestination(Touch3D.GetTouchWorldPosition());
+                        isDoubleTap = false;
+                    }
+                    else
+                    {
+                        // Reset double tap if the time threshold is exceeded
+                        doubleTapTimer = Time.time;
+                    }
+                }
+            }
+        }
+    }
+
+    public void SelectUnit()
+    {
+        if (selectedUnit)
+        {
+            selectedUnit.SetSelected(false);
+            selectedUnit = null;
+        }
+        else
+        {
+            GameObject mainCharacterObject = GameObject.FindGameObjectWithTag("MainCharacter");
+            if (mainCharacterObject != null)
+            {
+                selectedUnit = mainCharacterObject.GetComponent<Main_Character_Unit>();
+                if (selectedUnit != null)
+                {
                     selectedUnit.SetSelected(true);
                 }
             }
         }
-
-        if (Input.touchCount == 1){
-            selectedUnit.SetDestination(Touch3D.GetTouchWorldPosition());
-        }
-    }
-
-    private void DeselectUnit() {
-        selectedUnit.SetSelected(false);
-        selectedUnit = null;
     }
 }
